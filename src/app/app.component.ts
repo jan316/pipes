92a4ba2a25a932680289cc30a724ebbb3881c1f3
jan316 <tyllpet@gmail.com>
2022-06-11 18:35:51 +0200

    first commit app finished & running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: any;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: any) {
+    this.amount = parseFloat(value);
+  }
+
 }
